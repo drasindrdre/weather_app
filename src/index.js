5df4f1b9fd4f4a7003bc01b6f1e4c9b2ddad40c8
drasindrdre <selekobi@gmail.com>
2024-11-28 23:43:34 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -7,7 +7,36 @@ function refreshWeather(response) {
   cityElement.innerHTML = response.data.city;
 
   temperatureElement.innerHTML = Math.round(temperature);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+let windSpeedElement = document.querySelector("#wind-speed");
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+timeElement.innerHTML = formatDate(date);
+
 }
+
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let day = days[date.getDay()];
+
+if (minutes < 10) {
+    minutes = `0${minutes}` ;
+}
+
+return `${day} ${hours}:${minutes}`
+} 
 
 function searchCity(city) {
   let apiKey = "8a3a6608o4abb33fba96t64a93c2140f";
