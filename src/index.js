89d444665d95951e8a5d678a4a487e0884566c08
drasindrdre <selekobi@gmail.com>
2024-12-16 00:13:56 +0200

    api intergration

--- src/index.js
+++ src/index.js
@@ -66,38 +66,51 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let days = ['Sun', 'Mon' , 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
+return days[date.getDay()];
+}
+
+function getForecast(city) {
+let apiKey = "8a3a6608o4abb33fba96t64a93c2140f";
+let APIurl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios.get(APIurl).then(displayForecast);
+}
 
 function displayForecast(response) {
-let forecastElement = document.querySelector("#forecast");
-
-let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 let forecastHTML = "";
 
-days.forEach(function(day) {
-forecastHTML =  forecastHTML + `
+response.data.daily.forEach(function(day, index) {
+  if (index < 5) {
+forecastHTML =
+  forecastHTML +
+  `
   <div class = "weather-forecast-day" >
-    <div class = "weather-forecast-date">${day}</div>
-    <div class = "weather-forecast-icon">🌤️</div>
+    <div class = "weather-forecast-date">${formatDay(day.time)}</div>
+    
+    <img src = "${day.condition.icon_url}" class = "weather-forecast-icon"/>
+    
     <div class = "weather-forecast-temperatures">
       <div class = "weather-forecast-temperature">
-        <strong>15&deg;</strong>
+        <strong>${Math.round(day.temperature.maximum)}&deg;</strong>
       </div>
-      <div class = "weather-forecast-temperature">9&deg;</div>
+      <div class = "weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}&deg;</div>
     </div>
   </div>
 
-`;  
+`; 
+    } 
 });
 
+let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHTML;
 }
 
 displayForecast();
 
-function getForecast(city) {
-let apiKey = "8a3a6608o4abb33fba96t64a93c2140f";
-let apiURL = `https://api.shecodes.io/weather/v1/forecast?query = ${city}&key=${apiKey}&units=metric`;
-axios(apiURL),then(displayForecast);
-}
+
